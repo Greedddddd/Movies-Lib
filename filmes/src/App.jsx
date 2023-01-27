37a633fcdaabece61e7f433b6bbab534c5d53933
Caio Melo <101616140+Greedddddd@users.noreply.github.com>
2023-01-27 17:33:37 -0300

    quinto commit

--- filmes/src/App.jsx
+++ filmes/src/App.jsx
@@ -7,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
-      <h2>Movie libes</h2>
       <Outlet />
     </div>
   )
--- filmes/src/pages/Home.jsx
+++ filmes/src/pages/Home.jsx
@@ -21,7 +21,13 @@ const Home = () => {
     }, []);
 
     return(
-        <div>{topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}</div>
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando...</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+            </div>
+        </div>
      );
 };
 
