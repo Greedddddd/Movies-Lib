03e9d04510c5f2a2aea0b154b359ebaeb3affc15
Caio Melo <101616140+Greedddddd@users.noreply.github.com>
2023-01-28 14:21:26 -0300

    sexto commit

--- filmes/src/components/MovieCard.jsx
+++ filmes/src/components/MovieCard.jsx
@@ -7,9 +7,14 @@ const imageUrl = import.meta.env.VITE_IMG
 const MovieCard = ({movie, showLink = true}) => {
     return (
         <div className="movie-card">
-            <img src={imageUrl + movie.poster_path} alt="imagem" />
+            <img src={imageUrl + movie.poster_path} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>
+                <faStar />{movie.vote_average}       
+            </p>
+            {showLink && <Link to={`/movie/${movie.id}`}></Link>}
         </div>
-    )
-}
+    );
+};
 
 export default MovieCard 
