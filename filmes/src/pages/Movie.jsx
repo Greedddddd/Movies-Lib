060ab8a5d13d1dc601fd2cd65ba29d842177f58a
Caio Melo <101616140+Greedddddd@users.noreply.github.com>
2023-01-30 18:31:24 -0300

    1 terceiro commit

--- filmes/src/pages/Movie.jsx
+++ filmes/src/pages/Movie.jsx
@@ -1,6 +1,37 @@
+import { useState, useEffect } from "react";
+import { usaParams } from "react-router-dom";
+import {
+    BsGraphUp,
+    BsWallet2, 
+    BsHourgglassSplit, 
+    BsFillFileEarmarkTextFill
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "./Movie.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
+    const {id} = useParams()
+    const [movie, setMovie] = useState(null)
+
+    const getMovie = async(url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovie(data);
+    };
+
+    useEffect(() => {
+        const movieUrl = `${moviesURL}${id}?${apiKey}`;
+        getMovie(movieUrl);
+    }, [])
+
     return(
-        <div>Movie</div>
+        <div>{movie && <>{movie.title}</>}</div>
     )
 }
 
