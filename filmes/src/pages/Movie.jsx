3b5e7077b6d8199ed249c65a69be3b30680c7253
Caio Melo <101616140+Greedddddd@users.noreply.github.com>
2023-01-31 15:47:00 -0300

    commit final

--- filmes/src/pages/Movie.jsx
+++ filmes/src/pages/Movie.jsx
@@ -9,7 +9,7 @@ import {
 
 import MovieCard from "../components/MovieCard";
 
-import "./MovieGrid.css";
+import "./Movie.css";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -25,14 +25,60 @@ const Movie = () => {
         setMovie(data);
     };
 
+    const formatCurrency = (number) => {
+        return number.toLocaleString("en-US", {
+            style: "currency", 
+            currency: "USD"
+        })
+    }
+
     useEffect(() => {
         const movieUrl = `${moviesURL}${id}?${apiKey}`;
         getMovie(movieUrl);
     }, [])
 
     return(
-        <div>{movie && <>{movie.title}</>}</div>
-    )
-}
+        <div>
+            {movie && (
+            <>
+                <MovieCard movie={movie} showLink={false} />
+                <p className="tagline">{movie.tagline}</p>
+                <div className="info">
+                    <h3>
+                        <BsWallet2 /> Orçamento:
+                    </h3>
+                    <p>
+                       {formatCurrency(movie.budget)}
+                    </p>
+                </div>
+                <div className="info">
+                    <h3>
+                        <BsGraphUp /> Receita:
+                    </h3>
+                    <p>
+                        {formatCurrency(movie.revenue)}
+                    </p>
+                </div>
+                <div className="info">
+                    <h3>
+                        <BsHourglassSplit /> Duração
+                    </h3>
+                    <p>
+                        {movie.runtime} minutos
+                    </p>
+                </div>
+                <div className="info description">
+                    <h3>
+                        <BsFillFileEarmarkTextFill /> Descrição
+                    </h3>
+                    <p>
+                        {movie.overview}
+                    </p>
+                </div>
+            </>
+         )}
+        </div>
+    );
+};
 
 export default Movie
